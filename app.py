e40bbe90aadbaebb6014279c0da38f0135222c75
Siqi Liu <jsrglsq@gmail.com>
2024-12-21 13:16:01 +0800

    1. 优化水印样式排布
    2. 添加一个水印样式
    
    Signed-off-by: Siqi Liu <jsrglsq@gmail.com>

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def upload_file_served(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
