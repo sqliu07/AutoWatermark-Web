b2a927a843266938550f217663f130da293f5c5c
Siqi Liu <jsrglsq@gmail.com>
2025-02-14 23:52:42 +0800

    delete no use blanks
    Signed-off-by: Siqi Liu <jsrglsq@gmail.com>

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 class CommonConstants:
     #for app.py
     UPLOAD_FOLDER = './upload'
-    
+
     #for process.py
     GLOBAL_FONT_PATH_BOLD = "./fonts/AlibabaPuHuiTi-2-85-Bold.otf"
     GLOBAL_FONT_PATH_LIGHT = "./fonts/AlibabaPuHuiTi-2-45-Light.otf"
-    
+
     ERROR_MESSAGES = {
     "unsupported_manufacturer": {
         'en': "Unsupported camera! Please wait for our update.",
@@ -16,12 +16,10 @@ class CommonConstants:
         'zh': "该图片不包含有效的exif数据！"
     },
 }
-    
-    
+
 class ImageConstants:
     #for image_utils.py
     ORIGIN_BOTTOM_BORDER_RATIO = 0.08
     ORIGIN_TOP_BORDER_RATIO = 0.03
     LOGO_RATIO = 0.07
     ORIGIN_BOTTOM_HEIGHT = 250
-    
